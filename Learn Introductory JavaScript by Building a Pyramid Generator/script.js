95de80ea18fd8987ab03a51a079b09572e73f89a
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-04-28 08:36:05 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -9,5 +9,14 @@ Declare a character variable in your code.
 Note: It is common practice to end statements in JavaScript with a semicolon. ; 
 */
 
-let character;
+//let character;
 
+/*
+Step 2:
+Your character variable currently does not have a value. You can assign a value using the assignment operator =. For example:
+let hello = "Hello";
+Assigning a value to a variable at the moment of its declaration is known as initialization.
+Initialize your character variable by assigning it the value "Hello" during its declaration.
+*/
+
+let character = "Hello";

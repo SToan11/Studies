593cb31d1fa3e9df66fa67c89b8b881d13fae4ad
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-04-28 08:38:32 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1,7 +1,8 @@
 /*
 Step 1: 
 JavaScript is the programming language that powers the web. Unlike the HTML and CSS you have learned previously, JavaScript is most commonly used to write logic instead of markup.
-One of the most important concepts in programming is variables. A variable points to a specific memory address that stores a value. Variables are given a name which can be used throughout your code to access that value.
+One of the most important concepts in programming is variables. A variable points to a specific memory address that stores a value. 
+Variables are given a name which can be used throughout your code to access that value.
 Declaring a variable means giving it a name. In JavaScript, this is often done with the let keyword. For example, here is how you would declare a hello variable:
 let hello;
 Variable naming follows specific rules: names can include letters, numbers, dollar signs, and underscores, but cannot contain spaces and must not begin with a number.
@@ -19,4 +20,14 @@ Assigning a value to a variable at the moment of its declaration is known as ini
 Initialize your character variable by assigning it the value "Hello" during its declaration.
 */
 
-let character = "Hello";
+// let character = "Hello";
+
+/*
+Step 3:
+JavaScript has seven primitive data types, with String being one of them. In JavaScript, a string represents 
+a sequence of characters and can be enclosed in either single (') or double (") quotes.
+Note that strings are immutable, which means once they are created, they cannot be changed.
+Change your "Hello" string to use single quotes.
+*/
+
+let character = 'Hello';

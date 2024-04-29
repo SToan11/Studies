3ffda16b59e578a1034f6d89504a6b388a4665c7
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-04-29 13:30:01 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -81,8 +81,20 @@ Camel case means that the first word in the name is entirely lowercase, but the 
 Use camel case to declare a new secondCharacter variable.
 */
 
+// let character = 'Hello';
+// console.log(character);
+// character = "World";
+// console.log(character);
+// let secondCharacter;
+
+/*
+Step 8:
+When you declare a variable without initializing it, it is considered uninitialized. Currently, your secondCharacter variable is uninitialized.
+Add a console.log() to see what the value of your secondCharacter variable is.
+*/
+
 let character = 'Hello';
 console.log(character);
 character = "World";
-console.log(character);
 let secondCharacter;
+console.log(secondCharacter);

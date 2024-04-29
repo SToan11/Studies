fc0884b1556dddb133345aa107b25da209776ce7
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-04-29 13:31:19 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -93,8 +93,24 @@ When you declare a variable without initializing it, it is considered uninitiali
 Add a console.log() to see what the value of your secondCharacter variable is.
 */
 
+// let character = 'Hello';
+// console.log(character);
+// character = "World";
+// let secondCharacter;
+// console.log(secondCharacter);
+
+/*
+Step 9:
+The default value of an uninitialized variable is undefined. This is a special data type that represents a value that does not have a definition yet.
+You can still assign a value to an uninitialized variable. Here is an example:
+                let uninitialized;
+                uninitialized = "assigned";
+Assign the string "Test" to your secondCharacter variable below your declaration. Open the console to see how your log has changed.
+*/
+
 let character = 'Hello';
 console.log(character);
 character = "World";
 let secondCharacter;
+secondCharacter = "Test";
 console.log(secondCharacter);

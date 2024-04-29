b862742980fad1dc46ead3b5deec669265bb3433
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-04-29 13:28:48 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -4,7 +4,7 @@ JavaScript is the programming language that powers the web. Unlike the HTML and 
 One of the most important concepts in programming is variables. A variable points to a specific memory address that stores a value. 
 Variables are given a name which can be used throughout your code to access that value.
 Declaring a variable means giving it a name. In JavaScript, this is often done with the let keyword. For example, here is how you would declare a hello variable:
-        let hello;
+                let hello;
 Variable naming follows specific rules: names can include letters, numbers, dollar signs, and underscores, but cannot contain spaces and must not begin with a number.
 Declare a character variable in your code.
 Note: It is common practice to end statements in JavaScript with a semicolon. ; 
@@ -15,7 +15,7 @@ Note: It is common practice to end statements in JavaScript with a semicolon. ;
 /*
 Step 2:
 Your character variable currently does not have a value. You can assign a value using the assignment operator =. For example:
-        let hello = "Hello";
+                let hello = "Hello";
 Assigning a value to a variable at the moment of its declaration is known as initialization.
 Initialize your character variable by assigning it the value "Hello" during its declaration.
 */
@@ -36,8 +36,8 @@ Change your "Hello" string to use single quotes.
 Step 4:
 The console allows you to print and view JavaScript output. 
 You can send information to the console using console.log(). For example, this code will print "Naomi" to the console:
-        let developer = "Naomi";
-        console.log(developer);
+                let developer = "Naomi";
+                console.log(developer);
 The code above accesses the developer variable with its name in the console.log(). 
 Note that the value between the parentheses is the value that will be printed.
 Print the value of the character variable to the console. Then, click the "Console" button to view the JavaScript console.
@@ -50,8 +50,8 @@ Print the value of the character variable to the console. Then, click the "Conso
 Step 5:
 When a variable is declared with the let keyword, you can reassign (or change the value of) that variable later on. 
 In this example, the value of programmer is changed from "Naomi" to "CamperChan".
-            let programmer = "Naomi";
-            programmer = "CamperChan";
+                let programmer = "Naomi";
+                programmer = "CamperChan";
 Note that when reassigning a variable that has already been declared, you do not use the let keyword.
 After your console.log, assign the value "World" to your character variable.
 */
@@ -65,7 +65,24 @@ Step 6:
 Now log your character variable to the console again. You should see the string "Hello", then the string "World", in the console.
 */
 
+// let character = 'Hello';
+// console.log(character);
+// character = "World";
+// console.log(character);
+
+/*
+Step 7:
+When variable names are more than one word, there are specific naming conventions for how you capitalize the words. In JavaScript, the convention to use is camel case.
+Camel case means that the first word in the name is entirely lowercase, but the following words are all title-cased. Here are some examples of camel case:
+                let variableOne;
+                let secondVariable;
+                let yetAnotherVariable;
+                let thisIsAnAbsurdlyLongName;
+Use camel case to declare a new secondCharacter variable.
+*/
+
 let character = 'Hello';
 console.log(character);
 character = "World";
 console.log(character);
+let secondCharacter;

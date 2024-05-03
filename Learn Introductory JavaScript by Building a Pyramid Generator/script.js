2c4782f1bc51a78d010ec9198269f41f2e2e389c
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-03 17:46:36 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -165,5 +165,20 @@ You can also perform subtraction (-), multiplication (*), and division (/).
 Feel free to experiment with the operators and numbers in your console.log. When you are ready to move on, remove the console.log.
 */
 
+// let character = 'Hello';
+// let count = 8;
+
+/*
+Step 15:
+In programming, you will often need to work with lots of data. There are many data structures that can help you organize and manage your data. 
+One of the most basic data structures is an array.
+An array is a non-primitive data type that can hold a series of values. Non-primitive data types differ from primitive data types in that they can hold more complex data. 
+Primitive data types like strings and numbers can only hold one value at a time.
+Arrays are denoted using square brackets ([]). Here is an example of a variable with the value of an empty array:
+        let array = [];
+Declare a rows variable and assign it an empty array.
+*/
+
 let character = 'Hello';
 let count = 8;
+let rows = [];

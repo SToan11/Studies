dd095d78cb62a36999aa1310fbd82d263866def6
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-03 17:44:07 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -147,6 +147,15 @@ Use let to declare a count variable. Assign it the number 8. When using a number
         let money = 100;
 */
 
+// let character = 'Hello';
+// let count = 8;
+
+/*
+Step 13:
+With the number data type, you can perform mathematical operations, like addition. Try printing count + 1 to the console.
+*/
+
 let character = 'Hello';
 let count = 8;
+console.log(count + 1);
 

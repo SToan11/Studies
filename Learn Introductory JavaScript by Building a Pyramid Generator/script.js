d8132038afcf4ba4ab29b91456e4e750b6b815a2
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-04 16:09:44 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -202,7 +202,23 @@ You can access the value using bracket notation, such as array[0].
 Use console.log and bracket notation to print the first value in your rows array.
 */
 
+// let character = 'Hello';
+// let count = 8;
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+// console.log(rows[0]);
+
+/*
+Step 18:
+Arrays are special in that they are considered mutable. This means you can change the value at an index directly.
+For example, this code would assign the number 25 to the second element in the array:
+                let array = [1, 2, 3];
+                array[1] = 25;
+Update the third element of your rows array to be the number 10. Then print the rows array to your console.
+*/
+
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
 console.log(rows[0]);
+rows[2] = 10;
+console.log(rows);

84a6c65a1a01e6a1217599d65b86ab760bc98d00
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-04 16:05:03 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -191,7 +191,18 @@ Change your rows declaration to be an array with the strings "Naomi", "Quincy", 
 The order of values in an array is important, so follow that order. Remember that strings are case-sensitive.
 */
 
+// let character = 'Hello';
+// let count = 8;
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+
+/*
+Step 17:
+You can access the values inside an array using the index of the value. An index is a number representing the position of the value in the array, starting from 0 for the first value.
+You can access the value using bracket notation, such as array[0].
+Use console.log and bracket notation to print the first value in your rows array.
+*/
+
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
-
+console.log(rows[0]);

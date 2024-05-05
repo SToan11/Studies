09c26e0fc38156228550b0eaa4f6d80723d98245
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-05 16:48:23 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -307,6 +307,16 @@ It is important to be aware of what values a method returns. Take some time to e
 When you are ready, remove all of your .push() and .pop() calls, and your console.log statements.
 */
 
-let character = 'Hello';
+// let character = 'Hello';
+// let count = 8;
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+
+/*
+Step 25:
+Change your rows declaration to be assigned an empty array again.
+Also, change your 'Hello' string to use double quotes again. Generally, it does not matter which of the two you prefer, but you will want to be consistent in that choice throughout your project.
+*/
+
+let character = "Hello";
 let count = 8;
-let rows = ["Naomi", "Quincy", "CamperChan"];
+let rows = [];

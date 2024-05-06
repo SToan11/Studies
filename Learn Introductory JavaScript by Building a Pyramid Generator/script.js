46a98cc8edd82ebbc32027fb83af7e02826f55d4
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-06 21:52:18 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -333,6 +333,16 @@ A const variable also cannot be uninitialized. This code would throw an error:
 Replace your let keywords with const.
 */
 
-const character = "Hello";
+// const character = "Hello";
+// const count = 8;
+// const rows = [];
+
+/*
+Step 27:
+You are now ready to start building your pyramid generator. Your character variable will serve as the building block for the pyramid.
+"Hello" might not work very well for that. Change the value of character to be the hash character "#".
+*/
+
+const character = "#";
 const count = 8;
 const rows = [];

efbb82fe5adc788db0d7bdaf1aa18bad4cfe5ebc
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-06 21:44:42 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -317,6 +317,20 @@ Change your rows declaration to be assigned an empty array again.
 Also, change your 'Hello' string to use double quotes again. Generally, it does not matter which of the two you prefer, but you will want to be consistent in that choice throughout your project.
 */
 
-let character = "Hello";
+//let character = "Hello";
+//let count = 8;
+//let rows = [];
+
+/*
+Step 21:
+A method in JavaScript is a function that's associated with certain values or objects. An example you've already encountered is the .log() method, which is part of the console object.
+Arrays have their own methods, and the first you will explore is the .push() method. This allows you to "push" a value to the end of an array. Here is an example to add the number 12 to the end of an array:
+                array.push(12);
+Use .push() to add the string "freeCodeCamp" to the end of your rows array. Add this code before your console.log so you can see the change you made to your array.
+*/
+
+let character = 'Hello';
 let count = 8;
-let rows = [];
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);

6d59330956c1181b37809c56eaf55c70e4036af2
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-07 19:44:15 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -343,6 +343,27 @@ You are now ready to start building your pyramid generator. Your character varia
 "Hello" might not work very well for that. Change the value of character to be the hash character "#".
 */
 
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+/*
+Step 28:
+To generate a pyramid, you will need to create multiple rows. When you have to perform a task repeatedly until a condition is met, you will use a loop. There are many ways to write a loop.
+
+You are going to start with a basic for loop. for loops use the following syntax:
+
+                for (iterator; condition; iteration) {
+                logic;
+                }
+In the upcoming steps, you'll explore each component of a loop in detail. For now, construct a for loop that includes the terms "iterator", "condition", and "iteration" for the three components. 
+Keep the loop body, the section within the curly braces {}, empty.
+*/
+
 const character = "#";
 const count = 8;
 const rows = [];
+
+for ("iterator"; "condition"; "iteration") {
+
+}

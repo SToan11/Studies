bf4467eea29cf88e007d8c238489ea421a2bc9b6
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-08 21:24:00 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -431,6 +431,20 @@ You should see the numbers zero through seven printed in your console, one per l
 Replace your log statement with a statement to push i to your rows array.
 */
 
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(i);
+// }
+
+/*
+Step 34:
+Unfortunately, now you cannot see what your loop is doing.
+Use let to declare a result variable, and assign it an empty string. An empty string is represented by quotation marks with nothing between them, such as "".
+*/
+
 const character = "#";
 const count = 8;
 const rows = [];
@@ -438,4 +452,6 @@ const rows = [];
 for (let i = 0; i < count; i = i + 1) {
   rows.push(i);
 }
+
+let result = ""
 

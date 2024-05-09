eca8b7ef328729a2b51fcf06f66bef9c25ccbb42
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-09 19:26:20 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -461,6 +461,30 @@ Step 35:
 Add a log statement to print the value of result. Depending on which console you use, you may not see anything printed.
 */
 
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(i);
+// }
+
+// let result = ""
+
+// console.log(result);
+
+
+/*
+Step 36:
+To manipulate the result string, you will use a different type of loop. Specifically, a for...of loop, which iterates over each item in an iterable object and temporarily assigns it to a variable.
+The syntax for a for...of loop looks like:
+                for (const value of iterable) {
+
+                }
+Note that you can use const because the variable only exists for a single iteration, not during the entire loop.
+Create a for...of loop to iterate through your rows array, assigning each value to a row variable.
+*/
+
 const character = "#";
 const count = 8;
 const rows = [];
@@ -470,5 +494,9 @@ for (let i = 0; i < count; i = i + 1) {
 }
 
 let result = ""
+
+for (const row of rows) {
+
+}
 
 console.log(result);

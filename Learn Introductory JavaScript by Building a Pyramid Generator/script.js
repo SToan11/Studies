95b0f980a0e85f42035973312fba8650b7e722d6
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-12 21:34:29 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -661,6 +661,35 @@ In order to use a function, you need to call it. A function call tells your appl
 Call your padRow function.
 */
 
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+// function padRow() {
+
+// }
+// padRow();
+
+
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(character.repeat(i + 1))
+// }
+
+// let result = ""
+
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
+
+// console.log(result);
+
+
+/*
+Step 44:
+You are calling your padRow function, but not doing anything with that function call. All functions in JavaScript return a value, meaning they provide the defined result of calling them for you to use elsewhere.
+To see the result of calling your padRow function, declare a call variable and assign your existing padRow call to that variable.
+*/
+
 const character = "#";
 const count = 8;
 const rows = [];
@@ -668,7 +697,7 @@ const rows = [];
 function padRow() {
 
 }
-padRow();
+const call = padRow();
 
 
 for (let i = 0; i < count; i = i + 1) {

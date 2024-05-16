2edf4082a852b5cba52eebfab38046b7bfc2597c
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-16 19:38:51 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -952,18 +952,48 @@ There is a bit of a problem, here. Your test declaration is currently after your
 Move your test initialization to the line above your return statement.
 */
 
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+// function padRow(name) {
+//   const test = "Testing";
+//   return character + name;
+// }
+
+// const call = padRow("CamperChan");
+// console.log(call);
+
+
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(character.repeat(i + 1))
+// }
+
+// let result = ""
+
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
+
+// console.log(result);
+
+
+/*
+Step 53:
+If you try to add a console.log(test) call below your padRow function, you would see an error. This is because test is defined in the local scope, meaning you cannot access it in the global scope (outside of the padRow function).
+Returning a value from a function brings that value into the scope where the function was called. To bring your "Testing" value from the padRow function into the global scope, update your return statement to return only the test variable.
+*/
+
 const character = "#";
 const count = 8;
 const rows = [];
 
 function padRow(name) {
   const test = "Testing";
-  return character + name;
+  return test;
 }
-
 const call = padRow("CamperChan");
 console.log(call);
-
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1))

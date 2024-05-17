2d30af365e44a3398eda47aa065994d703b80f31
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-17 23:20:58 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -984,15 +984,44 @@ If you try to add a console.log(test) call below your padRow function, you would
 Returning a value from a function brings that value into the scope where the function was called. To bring your "Testing" value from the padRow function into the global scope, update your return statement to return only the test variable.
 */
 
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+// function padRow(name) {
+//   const test = "Testing";
+//   return test;
+// }
+// const call = padRow("CamperChan");
+// console.log(call);
+
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(character.repeat(i + 1))
+// }
+
+// let result = ""
+
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
+
+// console.log(result);
+
+/*
+Step 54:
+Now your call variable has the value "Testing". But your function is no longer using the name parameter.
+Remove the name parameter from your function declaration, then remove your "CamperChan" string from the padRow call.
+*/
+
 const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow(name) {
+function padRow() {
   const test = "Testing";
   return test;
 }
-const call = padRow("CamperChan");
+const call = padRow();
 console.log(call);
 
 for (let i = 0; i < count; i = i + 1) {

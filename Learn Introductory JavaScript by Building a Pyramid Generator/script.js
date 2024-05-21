de2f7a1ac2dd72935fa86e2df473ed76316c5fe6
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-21 23:20:34 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1098,3 +1098,24 @@ The best way to do this is by creating function parameters for them. Give your p
 
                 }
 */
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+
+}
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);

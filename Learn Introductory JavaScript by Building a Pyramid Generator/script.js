663e6e343671d40f66cd2b594ae245f575abe068
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-24 23:48:02 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1158,3 +1158,24 @@ A function is called by referencing the function's name, and adding (). Here's h
                 test();
 Replace the character.repeat(i + 1) in your .push() call with a function call for your padRow function.
 */
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return character.repeat(rowNumber);
+}
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow())
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);

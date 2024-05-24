092206770ad2e54cae624640f9e1a2bc44483fb0
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-24 23:47:34 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1130,23 +1130,31 @@ In a function, the return keyword is used to specify a return value. For example
 Use the return keyword to return the value of the character variable, repeated rowNumber times.
 */
 
-const character = "#";
-const count = 8;
-const rows = [];
+// const character = "#";
+// const count = 8;
+// const rows = [];
 
-function padRow(rowNumber, rowCount) {
-  return character.repeat(rowNumber);
-}
+// function padRow(rowNumber, rowCount) {
+//   return character.repeat(rowNumber);
+// }
 
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i + 1))
-}
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(character.repeat(i + 1))
+// }
 
-let result = ""
+// let result = ""
 
-for (const row of rows) {
-  result = result + "\n" + row;
-}
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
 
-console.log(result);
+// console.log(result);
+
+/*
+Step 59:
+A function call allows you to actually use a function. You may not have been aware of it, but the methods like .push() that you have been using have been function calls.
+A function is called by referencing the function's name, and adding (). Here's how to call a test function:
+                test();
+Replace the character.repeat(i + 1) in your .push() call with a function call for your padRow function.
+*/

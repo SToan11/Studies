d12525aebde464ad25ce18579c0393a755c2a0ca
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-30 23:31:04 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1243,3 +1243,23 @@ The values you provide to a function call are referred to as arguments, and you 
                 test("Hello");
 Pass i + 1 and count as the arguments to your padRow call. Like parameters, arguments are separated by a comma.
 */
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return character.repeat(rowNumber);
+}
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);

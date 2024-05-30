4801159f77e6a99699e8b4101b807c5e734f047d
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-30 23:30:32 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1216,22 +1216,30 @@ Remember that you can use the + operator to concatenate strings like this:
                 " " + "string"
 */
 
-const character = "#";
-const count = 8;
-const rows = [];
+// const character = "#";
+// const count = 8;
+// const rows = [];
 
-function padRow(rowNumber, rowCount) {
-  return " " + character.repeat(rowNumber) + " ";
-}
+// function padRow(rowNumber, rowCount) {
+//   return " " + character.repeat(rowNumber) + " ";
+// }
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
-}
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(padRow(i + 1, count));
+// }
 
-let result = ""
+// let result = ""
 
-for (const row of rows) {
-  result = result + "\n" + row;
-}
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
 
-console.log(result);
+// console.log(result);
+
+/*
+Step 62:
+Your padRow function has two parameters which you defined. Values are provided to those parameters when a function is called.
+The values you provide to a function call are referred to as arguments, and you pass arguments to a function call. Here's a function call with "Hello" passed as an argument:
+                test("Hello");
+Pass i + 1 and count as the arguments to your padRow call. Like parameters, arguments are separated by a comma.
+*/

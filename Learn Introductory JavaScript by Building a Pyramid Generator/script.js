60e488c5e31bdbc806759362b95138431d320bef
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-05-31 22:21:37 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1244,22 +1244,31 @@ The values you provide to a function call are referred to as arguments, and you 
 Pass i + 1 and count as the arguments to your padRow call. Like parameters, arguments are separated by a comma.
 */
 
-const character = "#";
-const count = 8;
-const rows = [];
+// const character = "#";
+// const count = 8;
+// const rows = [];
 
-function padRow(rowNumber, rowCount) {
-  return character.repeat(rowNumber);
-}
+// function padRow(rowNumber, rowCount) {
+//   return character.repeat(rowNumber);
+// }
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
-}
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(padRow(i + 1, count));
+// }
 
-let result = ""
+// let result = ""
 
-for (const row of rows) {
-  result = result + "\n" + row;
-}
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
 
-console.log(result);
+// console.log(result);
+
+
+/*
+Step 63:
+You should now see the same bunch of characters in your console. Your padRow function is doing the exact same thing you were doing earlier, but now it's in a reusable section of its own.
+Use the addition operator to concatenate a blank space " " to the beginning and end of your repeated character string.
+Remember that you can use the + operator to concatenate strings like this:
+                " " + "string"
+*/

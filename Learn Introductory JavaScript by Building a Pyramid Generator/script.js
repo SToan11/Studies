aa8c0011356c26006642ee90c55809b96983f9bc
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-06-02 22:11:32 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1303,3 +1303,23 @@ Empty spaces have been replaced with interpuncts, or middle dots, for readabilit
 Update your blank space strings to be repeated rowCount - rowNumber times.
 Open up the console to see the result.
 */
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);

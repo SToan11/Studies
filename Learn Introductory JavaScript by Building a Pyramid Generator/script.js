cbeaa1a44b179035a103f56b437eec48134f328e
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-06-05 23:06:15 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1337,6 +1337,35 @@ Update the character value to be repeated 2 * rowNumber - 1 times.
 Open up the console again to see the updated result.
 */
 
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// }
+
+// for (let i = 0; i < count; i = i + 1) {
+//   rows.push(padRow(i + 1, count));
+// }
+
+// let result = ""
+
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
+
+// console.log(result);
+
+/*
+Step 66:
+Your pyramid generator now functions as expected. But this is an excellent opportunity to further explore the code you have written.
+The addition operator is not the only way to add values to a variable. The addition assignment operator can be used as shorthand to mean "take the original value of the variable, add this value, and assign the result back to the variable." For example, these two statements would yield the same result:
+                test = test + 1;
+                test += 1;
+Update your iterator statement in the for loop to use addition assignment.
+*/
+
 const character = "#";
 const count = 8;
 const rows = [];
@@ -1345,7 +1374,7 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
+for (let i = 0; i < count; i += 1) {
   rows.push(padRow(i + 1, count));
 }
 
@@ -1356,12 +1385,3 @@ for (const row of rows) {
 }
 
 console.log(result);
-
-/*
-Step 66:
-Your pyramid generator now functions as expected. But this is an excellent opportunity to further explore the code you have written.
-The addition operator is not the only way to add values to a variable. The addition assignment operator can be used as shorthand to mean "take the original value of the variable, add this value, and assign the result back to the variable." For example, these two statements would yield the same result:
-                test = test + 1;
-                test += 1;
-Update your iterator statement in the for loop to use addition assignment.
-*/

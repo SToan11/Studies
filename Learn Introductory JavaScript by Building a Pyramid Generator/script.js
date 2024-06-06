1a4e5d589c6fb5e2438d5ce3a0d26c0eef623154
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-06-06 21:38:24 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1366,6 +1366,34 @@ The addition operator is not the only way to add values to a variable. The addit
 Update your iterator statement in the for loop to use addition assignment.
 */
 
+// const character = "#";
+// const count = 8;
+// const rows = [];
+
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// }
+
+// for (let i = 0; i < count; i += 1) {
+//   rows.push(padRow(i + 1, count));
+// }
+
+// let result = ""
+
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
+
+// console.log(result);
+
+/*
+Step 67:
+Because you are only increasing i by 1, you can use the increment operator ++. This operator increases the value of a variable by 1, updating the assignment for that variable. For example, test would become 8 here:
+                let test = 7;
+                test++;
+Replace your addition assignment with the increment operator for your loop iteration.
+*/
+
 const character = "#";
 const count = 8;
 const rows = [];
@@ -1374,7 +1402,7 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i += 1) {
+for (let i = 0; i < count; i++) {
   rows.push(padRow(i + 1, count));
 }
 

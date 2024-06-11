dffa300218de55aba44097e03295543165ce69d4
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-06-11 21:31:12 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1499,8 +1499,27 @@ Update your loop condition to run while i is less than or equal to count.
 /*
 Step 71:
 Comments can be helpful for explaining why your code takes a certain approach, or leaving to-do notes for your future self.
-
 In JavaScript, you can use // to leave a single-line comment in your code.
-
 Add a single-line comment above your function to remind yourself to change the code to a different kind of loop.
 */
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);

ce9f55665c7e5b277054c2be659f88df2bc73ba6
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-06-13 23:31:44 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1530,3 +1530,22 @@ Rather than having to pass i + 1 to your padRow call, you could instead start yo
 Update your iterator to start at 1 instead of 0.
 */
 
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i < count; i++) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);

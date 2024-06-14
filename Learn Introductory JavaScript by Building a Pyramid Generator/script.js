a4e9c288e4405b58f7f4f0bab1b66408ff8f7374
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-06-14 23:14:06 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1557,3 +1557,22 @@ The pyramid looks a little funny now. Because you are starting the loop at 1 ins
 Update the first argument of your padRow call to be i.
 */
 
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i < count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);

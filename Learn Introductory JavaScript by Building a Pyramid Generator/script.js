c47368a1ff477f2ccbe248072dc6d9e32e2f0aac
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-06-14 23:13:55 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1530,22 +1530,30 @@ Rather than having to pass i + 1 to your padRow call, you could instead start yo
 Update your iterator to start at 1 instead of 0.
 */
 
-const character = "#";
-const count = 8;
-const rows = [];
+// const character = "#";
+// const count = 8;
+// const rows = [];
 
-function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-}
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// }
 
-for (let i = 1; i < count; i++) {
-  rows.push(padRow(i + 1, count));
-}
+// for (let i = 1; i < count; i++) {
+//   rows.push(padRow(i + 1, count));
+// }
 
-let result = ""
+// let result = ""
 
-for (const row of rows) {
-  result = result + "\n" + row;
-}
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
 
-console.log(result);
+// console.log(result);
+
+
+/*
+Step 73:
+The pyramid looks a little funny now. Because you are starting the loop at 1 instead of 0, you do not need to add one to i when you pass it to padRow.
+Update the first argument of your padRow call to be i.
+*/
+

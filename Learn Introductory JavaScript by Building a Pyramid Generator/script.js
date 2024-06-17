930824863dfc614f1ddaf83cd498f0ab909b8858
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-06-17 23:41:49 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1635,8 +1635,31 @@ Add a single-line comment above your function to remind yourself to change the c
 // console.log(result);
 
 /*
-Step 76Passed
+Step 76:
 JavaScript also has support for multi-line comments. A multi-line comment starts with /* and ends with *//*
 Unlike a single-line comment, a multi-line comment will encapsulate multiple lines.
 Use /* and */ /*to turn your current for loop, including the body, into a multi-line comment.
 */
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);

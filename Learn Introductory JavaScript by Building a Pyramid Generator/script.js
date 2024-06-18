47195eb55704cdaab12a53886ceb0fb05d2003ea
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-06-18 23:32:25 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1674,3 +1674,28 @@ Before you create your new loop, you need to learn about if statements. An if st
                 }
 Create an if statement with the boolean true as the condition. In the body, print the string "Condition is true".
 */
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if (true) {
+  console.log("Condition is true");
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);

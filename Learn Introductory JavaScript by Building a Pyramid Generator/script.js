77e8de5c25a5dd8c77c9e17cfbadececcff52cd3
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-06-24 23:44:31 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1895,3 +1895,23 @@ for (const row of rows) {
 }
 
 console.log(result);
+
+
+/*
+Step 82:
+Sometimes you will want to run different code when all of the if...else if conditions are false. You can do this by adding an else block.
+An else block will only evaluate if the conditions in the if and else if blocks are not met.
+Here the else block is added to the else if block.
+Example Code
+              if (condition) {
+                // this code will run if condition is true
+              } else if (condition2) {
+                // this code will run if the first condition is false
+              } else {
+                // this code will run 
+                // if the first and second conditions are false
+              }
+Add an else block to the else if block. Inside the else block, log the string "This is the else block" to the console.
+To see the results in the console, you can manually change the < in the else if statement to >. That will make the condition false and the else block will run.
+*/
+

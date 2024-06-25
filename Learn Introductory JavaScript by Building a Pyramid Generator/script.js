a54bfc0f06050685c18f73eb57e94d9c73d6139f
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-06-25 23:32:38 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1903,27 +1903,38 @@ Now that you have practiced working with if...else if...else statements, you can
 Once you complete that, use let to declare a continueLoop variable and assign it the boolean false. Then use let to declare a done variable and assign it the value 0.
 */
 
-const character = "#";
-const count = 8;
-const rows = [];
+// const character = "#";
+// const count = 8;
+// const rows = [];
 
-function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-}
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
+// // TODO: use a different type of loop
+// /*for (let i = 1; i <= count; i++) {
+//   rows.push(padRow(i, count));
+// }*/
 
-let continueLoop = false;
-let done = 0;
+// let continueLoop = false;
+// let done = 0;
 
 
-let result = ""
+// let result = ""
 
-for (const row of rows) {
-  result = result + "\n" + row;
-}
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
 
-console.log(result);
+// console.log(result);
+
+
+/*
+Step 84:
+A while loop will run over and over again until the condition specified is no longer true. It has the following syntax:
+Example Code
+                while (condition) {
+                  logic;
+                }
+Use that syntax to declare a while loop with continueLoop as the condition. The body should be empty.
+*/

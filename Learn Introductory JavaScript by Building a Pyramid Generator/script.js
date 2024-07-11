c80dc8c34fe478a950185972aa85185ee443d09f
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-07-11 23:03:04 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2350,30 +2350,39 @@ Update your condition to check if rows.length is less than or equal to count.
 */
 
 
-const character = "#";
-const count = 8;
-const rows = [];
+// const character = "#";
+// const count = 8;
+// const rows = [];
 
-function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-}
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
+// // TODO: use a different type of loop
+// /*for (let i = 1; i <= count; i++) {
+//   rows.push(padRow(i, count));
+// }*/
 
-let done = 0;
+// let done = 0;
 
-while (rows.length <= count) {
-  done++;
-  rows.push(padRow(done, count));
-}
+// while (rows.length <= count) {
+//   done++;
+//   rows.push(padRow(done, count));
+// }
 
-let result = ""
+// let result = ""
 
-for (const row of rows) {
-  result = result + "\n" + row;
-}
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
 
-console.log(result);
+// console.log(result);
+
+
+/*
+Step 96:
+You can also replace the done reference in your padRow call.
+Note that rows.length here would give you an off-by-one error, because done is incremented before the call.
+So you'll need to replace done here with rows.length + 1. When you do this, you may see a Range Error, because we've created another off-by-one error.
+You'll need to change the while condition to use the less than operator, instead of the less than or equal operator.
+*/

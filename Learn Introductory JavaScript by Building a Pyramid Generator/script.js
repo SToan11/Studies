8041a0468549b54869ef2fee71fbff79e25821bc
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-07-18 22:01:49 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2527,31 +2527,40 @@ Set your loop's condition to run when i is greater than 0.
 */
 
 
-const character = "#";
-const count = 8;
-const rows = [];
+// const character = "#";
+// const count = 8;
+// const rows = [];
 
-function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-}
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
+// // TODO: use a different type of loop
+// /*for (let i = 1; i <= count; i++) {
+//   rows.push(padRow(i, count));
+// }*/
 
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
+// /*while (rows.length < count) {
+//   rows.push(padRow(rows.length + 1, count));
+// }*/
 
-for (let i = count; i > 0; false) {
+// for (let i = count; i > 0; false) {
 
-}
+// }
 
-let result = ""
+// let result = ""
 
-for (const row of rows) {
-  result = result + "\n" + row;
-}
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
 
-console.log(result);
+// console.log(result);
+
+
+/*
+Step 101:
+Your iteration statement is also going to be different. Instead of adding 1 to i with each loop, you need to subtract 1.
+Like you did earlier with i = i + 1, update your iteration statement to give i the value of subtracting 1 from itself.
+*/
+
+

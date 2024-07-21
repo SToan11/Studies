1c56c5a5e1993537fb9b67bde9bba82781129097
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-07-21 22:23:50 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2563,31 +2563,38 @@ Your iteration statement is also going to be different. Instead of adding 1 to i
 Like you did earlier with i = i + 1, update your iteration statement to give i the value of subtracting 1 from itself.
 */
 
-const character = "#";
-const count = 8;
-const rows = [];
+// const character = "#";
+// const count = 8;
+// const rows = [];
 
-function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-}
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
+// // TODO: use a different type of loop
+// /*for (let i = 1; i <= count; i++) {
+//   rows.push(padRow(i, count));
+// }*/
 
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
+// /*while (rows.length < count) {
+//   rows.push(padRow(rows.length + 1, count));
+// }*/
 
-for (let i = count; i > 0; i = i - 1) {
+// for (let i = count; i > 0; i = i - 1) {
 
-}
+// }
 
-let result = ""
+// let result = ""
 
-for (const row of rows) {
-  result = result + "\n" + row;
-}
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
 
-console.log(result);
+// console.log(result);
+
+
+/*
+Step 102:
+Again, push the result of calling padRow with your i and count variables to your rows array.
+Open up the console to see the upside-down pyramid.
+*/

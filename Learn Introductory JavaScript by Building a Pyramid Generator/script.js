6f39bd42c3364343b5b0bbccb58bb429b5d9df28
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-08-26 20:48:57 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -3338,31 +3338,38 @@ Uncomment only your first for loop. Leave the single line comment and the other 
 
 
 
-const character = "#";
-const count = 8;
-const rows = [];
+// const character = "#";
+// const count = 8;
+// const rows = [];
 
-function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-}
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// }
 
-// TODO: use a different type of loop
-for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}
+// // TODO: use a different type of loop
+// for (let i = 1; i <= count; i++) {
+//   rows.push(padRow(i, count));
+// }
 
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
+// /*while (rows.length < count) {
+//   rows.push(padRow(rows.length + 1, count));
+// }*/
 
-/*for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}*/
+// /*for (let i = count; i > 0; i--) {
+//   rows.push(padRow(i, count));
+// }*/
 
-let result = ""
+// let result = ""
 
-for (const row of rows) {
-  result = result + "\n" + row;
-}
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
 
-console.log(result);
+// console.log(result);
+
+
+/*
+Step 111:
+Your pyramid is no longer inverted. This is because you are adding new rows to the end of the array.
+Update your loop body to add new rows to the beginning of the array.
+*/

e59d266bd45fdc7ff7b72ed0c95f28fe13848681
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-08-30 19:39:44 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -3414,34 +3414,40 @@ Start by declaring an inverted variable, and assigning it the value true. You ar
 */
 
 
-const character = "#";
-const count = 8;
-const rows = [];
-let inverted = true;
+// const character = "#";
+// const count = 8;
+// const rows = [];
+// let inverted = true;
 
-function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-}
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// }
 
-// TODO: use a different type of loop
-for (let i = 1; i <= count; i++) {
+// // TODO: use a different type of loop
+// for (let i = 1; i <= count; i++) {
 
-  rows.unshift(padRow(i, count));
-}
+//   rows.unshift(padRow(i, count));
+// }
 
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
+// /*while (rows.length < count) {
+//   rows.push(padRow(rows.length + 1, count));
+// }*/
 
-/*for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}*/
+// /*for (let i = count; i > 0; i--) {
+//   rows.push(padRow(i, count));
+// }*/
 
-let result = ""
+// let result = ""
 
-for (const row of rows) {
-  result = result + "\n" + row;
-}
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
 
-console.log(result);
+// console.log(result);
 
+
+
+/*
+Step 113:
+Use an if statement to check if inverted is true. Remember that you do not need to use an equality operator here.
+*/

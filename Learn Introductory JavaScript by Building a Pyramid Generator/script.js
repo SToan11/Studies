04346a92b7f3295ee2c5eaeb422126955b9af92a
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-09-12 23:42:31 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -3539,3 +3539,45 @@ In earlier steps, you learned how to work with else statement like this:
                 }
 Add an else block to your if block.
 */
+
+
+// const character = "#";
+// const count = 8;
+// const rows = [];
+// let inverted = true;
+
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// }
+
+// // TODO: use a different type of loop
+// for (let i = 1; i <= count; i++) {
+//   if (inverted) {
+//     rows.unshift(padRow(i, count));
+//   } else {
+
+//   }
+// }
+
+// /*while (rows.length < count) {
+//   rows.push(padRow(rows.length + 1, count));
+// }*/
+
+// /*for (let i = count; i > 0; i--) {
+//   rows.push(padRow(i, count));
+// }*/
+
+// let result = ""
+
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
+
+// console.log(result);
+
+
+/*
+Step 116:
+When inverted is false, you want to build a standard pyramid. Use .push() like you have in previous steps to achieve this.
+*/
+

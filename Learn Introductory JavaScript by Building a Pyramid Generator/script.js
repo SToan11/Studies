7782da6100127c46621fffaeda2d6cd96a41fc0d
SToan11 <143256909+SToan11@users.noreply.github.com>
2024-11-06 15:36:17 +0700

    Learn Introductory JS

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -3582,36 +3582,43 @@ When inverted is false, you want to build a standard pyramid. Use .push() like y
 */
 
 
-const character = "#";
-const count = 8;
-const rows = [];
-let inverted = true;
+// const character = "#";
+// const count = 8;
+// const rows = [];
+// let inverted = true;
 
-function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-}
+// function padRow(rowNumber, rowCount) {
+//   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+// }
 
-// TODO: use a different type of loop
-for (let i = 1; i <= count; i++) {
-  if (inverted) {
-    rows.unshift(padRow(i, count));
-  } else {
-    rows.push(padRow(i, count));
-  }
-}
+// // TODO: use a different type of loop
+// for (let i = 1; i <= count; i++) {
+//   if (inverted) {
+//     rows.unshift(padRow(i, count));
+//   } else {
+//     rows.push(padRow(i, count));
+//   }
+// }
 
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
+// /*while (rows.length < count) {
+//   rows.push(padRow(rows.length + 1, count));
+// }*/
 
-/*for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}*/
+// /*for (let i = count; i > 0; i--) {
+//   rows.push(padRow(i, count));
+// }*/
 
-let result = ""
+// let result = ""
 
-for (const row of rows) {
-  result = result + "\n" + row;
-}
+// for (const row of rows) {
+//   result = result + "\n" + row;
+// }
 
-console.log(result);
+// console.log(result);
+
+
+/*
+Step 117:
+Your pyramid generator is now in a finished state, with more functionality than you originally planned! The next step is to clean up your code.
+Remove all comments, both single- and multi-line, from your code.
+*/
